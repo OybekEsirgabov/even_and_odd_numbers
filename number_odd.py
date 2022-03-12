54d41c7b9bc1684d31042294f36739a93fe54e74
tillabola <90684441+tillabola@users.noreply.github.com>
2022-03-12 09:57:37 +0500

    Update number_odd.py
    o'zgardi

--- number_odd.py
+++ number_odd.py
@@ -1,11 +1,23 @@
 #A four-digit integer is given. Find the number of odd digits in it.
 
 #Create a variable "var_int" and assign it a four-digit integer value.
+var_int = 5454
+#Print the number of even digits in the variable "var_int".
+s = 0
+x1 = var_int % 10
+var_int //= 10
+s += x1*(x1%2)
 
-#Print the number of odd digits in the variable "var_int".
-a = 4673
-b = a//1000
-c = a//100-b*10
-d = a//10-c*10-b*100
-e = a-b*1000-c*100-d*10
-print((e+1)%2+(d+1)%2+(c+1)%2+(b+1)%2)
+x2 = var_int % 10
+var_int //= 10
+s += x2*(x2%2)
+
+x3 = var_int % 10
+var_int //= 10
+s += x3*(x3%2)
+
+x4 = var_int % 10
+var_int //= 10
+s += x4*(x4%2)
+
+print(s)
